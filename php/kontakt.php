9d8e3f82de720d1cd2efe599be0eca290d8236db
andremeronen1 <andremeronen@gmail.com>
2026-05-12 12:33:45 +0300

    ye

--- php/kontakt.php
+++ php/kontakt.php
@@ -11,6 +11,34 @@
 
 <div class="container mt-5">
 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">Autorent</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="menu">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Avaleht</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tooted.php">Tooted</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kalkulaator.php">Kalkulaator</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kontakt.php">Kontakt</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="ostukorv.php">Ostukorv</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
     <h1>Kontakt</h1>
 
     <form>
